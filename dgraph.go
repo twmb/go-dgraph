ae0a6fc979e5150a7491b30f45208c58e58a523c
Travis Bischel <twmb@protonmail.com>
2019-08-17 03:30:04 -0400

    increase perf of SCC

--- dgraph.go
+++ dgraph.go
@@ -55,51 +55,77 @@ func (g *Graph) Link(src, dst int) {
 // Note that this is a topological sort: if a graph has no cycles, the order of
 // the returned single element components is the dependency order of the graph.
 func (g *Graph) StrongComponents() [][]int {
-	flip := make([]bool, len(g.out))
-	saw := func(node int) bool {
-		r := flip[node]
-		flip[node] = true
-		return r
+	d := dfser{
+		graph: g.out,
+		order: make([]int, 0, len(g.out)),
+		flip:  make([]bool, len(g.out)),
 	}
 
-	order := make([]int, 0, len(g.out))
 	for node := range g.out {
-		if !saw(node) {
-			dfs(g.out, node, saw, func(ord int) { order = append(order, ord) })
+		if !d.sawTrue(node) {
+			d.dfs1(node)
 		}
 	}
 
-	saw = func(node int) bool {
-		r := flip[node]
-		flip[node] = false
-		return !r
-	}
+	order := d.order
+
+	d.order = make([]int, 0, len(order))
+	d.graph = g.in
 
 	components := make([][]int, 0, len(order))
-	nodes := make([]int, 0, len(order))
 	for i := len(order) - 1; i >= 0; i-- {
-		if node := order[i]; !saw(node) {
-			dfs(g.in, node, saw, func(ord int) { nodes = append(nodes, ord) })
-			used := len(nodes)
-			component := nodes[:used:used]
+		if node := order[i]; !d.sawFalse(node) {
+			d.dfs2(node)
+			used := len(d.order)
+			component := d.order[:used:used]
 			components = append(components, component)
-			nodes = nodes[used:]
+			d.order = d.order[used:]
 		}
 	}
 
 	return components
 }
 
-func dfs(
-	graph [][]int,
-	node int,
-	saw func(int) bool,
-	fn func(int),
-) {
-	for _, neighbor := range graph[node] {
-		if !saw(neighbor) {
-			dfs(graph, neighbor, saw, fn)
+// dfser performs depth first search on the graph, appending bottom nodes to
+// order.
+//
+// Since SCC requires two dfs runs, we keep track of nodes seen with flip: on
+// the first run, we swap flip nodes to true, on the second back to false.
+//
+// The original impl just passed graph and two closures to one dfs function,
+// but switching to methods on a struct proved a big (>30%) performance
+// increase.
+type dfser struct {
+	graph [][]int
+	order []int
+	flip  []bool
+}
+
+func (d *dfser) sawTrue(node int) bool {
+	r := d.flip[node]
+	d.flip[node] = true
+	return r
+}
+func (d *dfser) sawFalse(node int) bool {
+	r := d.flip[node]
+	d.flip[node] = false
+	return !r
+}
+
+func (d *dfser) dfs1(node int) {
+	for _, neighbor := range d.graph[node] {
+		if !d.sawTrue(neighbor) {
+			d.dfs1(neighbor)
 		}
 	}
-	fn(node)
+	d.order = append(d.order, node)
+}
+
+func (d *dfser) dfs2(node int) {
+	for _, neighbor := range d.graph[node] {
+		if !d.sawFalse(neighbor) {
+			d.dfs2(neighbor)
+		}
+	}
+	d.order = append(d.order, node)
 }
